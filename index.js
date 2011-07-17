5ba068b229c8f3a1b93f0c7c24db70498d707834
James Halliday <mail@substack.net>
2011-07-16 22:32:59 -0700

    all tests pass now with contiguous tests

--- index.js
+++ index.js
@@ -3,14 +3,26 @@ var traverse = require('traverse');
 var quack = module.exports = function (obj) {
     if (typeof obj !== 'object') return obj;
     
+    if (!obj.hasOwnProperty('0')) return obj;
     var keys = Object.keys(obj);
-    if (keys.every(function (key) {
+    if (keys.length === 0) return obj;
+    
+    var allNumbers = keys.every(function (key) {
         return key.match(/^\d+$/);
-    })) {
-        obj.length = keys.length;
-        return [].slice.call(obj);
+    });
+    if (!allNumbers) return obj;
+    
+    var max = Math.max.apply(null, keys);
+    
+    for (var i = 0; i < keys.length; i++) {
+        var key = parseInt(keys[i], 10);
+        if (key !== max && !obj.hasOwnProperty(key+1)) {
+            return obj; // not contiguous
+        }
     }
-    else return obj;
+    
+    obj.length = keys.length;
+    return [].slice.call(obj);
 };
 
 quack.deep = function (obj) {
